e8ec21570f596a5036f29644ecd0df972ff016f7
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-04-27 06:50:25 +0900

    [UPDATE]Test_rails

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,7 +3,9 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
+
   def create
+    # https://web-camp.online/lesson/curriculums/212/contents/1435
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
@@ -17,4 +19,8 @@ class TodolistsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body)
   end
+#   list_paramsではフォームで入力されたデータを受け取っています。細かく見ていきましょう。
+# paramsはRailsで送られてきた値を受け取るためのメソッドです。
+# requireでデータのオブジェクト名(ここでは:list)を指定し、permitでキー（:title,:body）を指定しています。
+# これにより、list_paramsの中にフォームで入力されたデータが格納されます。
 end
