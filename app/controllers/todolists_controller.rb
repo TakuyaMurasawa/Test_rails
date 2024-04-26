c9ddb5cd60be37534f0d3de3c31f855a8f86ecd2
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2024-04-27 08:47:56 +0900

    [UPDATE]Test_rails/index

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -7,11 +7,16 @@ class TodolistsController < ApplicationController
   def create
     # https://web-camp.online/lesson/curriculums/212/contents/1435
     # １. データを新規登録するためのインスタンス作成
+    # ここのlistに@がないのは余計な事故を防ぐため
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # ３. トップ画面へリダイレクト
     redirect_to '/top'
+  end
+
+  def index
+    @lists = List.all
   end
 
   private
